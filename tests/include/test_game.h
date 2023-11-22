569a3bf9174ed18644f021e36135bb991ed20504
Irvin Cote <irvincoteg@gmail.com>
2023-11-22 16:55:24 +0100

    Adding unit tests for exists_move_that_feeds_opponent

--- tests/include/test_game.h
+++ tests/include/test_game.h
@@ -7,5 +7,9 @@ bool test_is_starved_opponent_player_one_should_return_true(void);
 bool test_is_starved_opponent_player_one_should_return_false(void);
 bool test_is_starved_opponent_player_two_should_return_true(void);
 bool test_is_starved_opponent_player_two_should_return_false(void);
+bool test_exists_move_that_feeds_opponent_player_one_should_return_true(void);
+bool test_exists_move_that_feeds_opponent_player_one_should_return_false(void);
+bool test_exists_move_that_feeds_opponent_player_two_should_return_true(void);
+bool test_exists_move_that_feeds_opponent_player_two_should_return_false(void);
 
 #endif
--- tests/test_game.c
+++ tests/test_game.c
@@ -2,6 +2,7 @@
 #include "../src/game_types.h"
 
 bool is_starved_opponent(struct board * b);
+bool exists_move_that_feeds_opponent(struct board * b);
 
 bool test_is_starved_opponent_player_one_should_return_true() {
 	struct board b = {
@@ -33,4 +34,36 @@ bool test_is_starved_opponent_player_two_should_return_false() {
 	.to_play = PLAYER2
 	};
 	return (is_starved_opponent(&b) == false);
+}
+
+bool test_exists_move_that_feeds_opponent_player_one_should_return_true() {
+	struct board b = {
+	.holes = {5,0,3,0,2,0,0,0,0,0,0,0},
+	.to_play = PLAYER1
+	};
+	return (exists_move_that_feeds_opponent(&b) == true);
+}
+
+bool test_exists_move_that_feeds_opponent_player_one_should_return_false() {
+	struct board b = {
+	.holes = {5,0,3,0,1,0,0,0,0,0,0,0},
+	.to_play = PLAYER1
+	};
+	return (exists_move_that_feeds_opponent(&b) == false);
+}
+
+bool test_exists_move_that_feeds_opponent_player_two_should_return_true() {
+	struct board b = {
+	.holes = {0,0,0,0,0,0,1,0,4,0,0,0},
+	.to_play = PLAYER2
+	};
+	return (exists_move_that_feeds_opponent(&b) == true);
+}
+
+bool test_exists_move_that_feeds_opponent_player_two_should_return_false() {
+	struct board b = {
+	.holes = {0,0,0,0,0,0,1,0,3,0,1,0},
+	.to_play = PLAYER2
+	};
+	return (exists_move_that_feeds_opponent(&b) == false);
 }
